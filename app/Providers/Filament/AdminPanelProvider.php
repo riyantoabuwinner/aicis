194391e1cd466fa2289bca9b5d1034e6f72a6d90
riyantoabuwinner <riyantoabuwinner@example.com>
2026-08-05 17:32:14 +0700

    style: remove filament info widget from dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -66,7 +66,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
                 \App\Filament\Widgets\AuthorGuidelinesWidget::class,
             ])
             ->navigationGroups([
