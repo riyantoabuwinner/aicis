f8124baf009de5e9cdc60e747305bf44c3cb9c6a
riyantoabuwinner <riyantoabuwinner@example.com>
2026-08-05 23:52:40 +0700

    style: increase topbar welcome message font size and align left

--- resources/views/partials/welcome-topbar.blade.php
+++ resources/views/partials/welcome-topbar.blade.php
@@ -1,6 +1,6 @@
 @auth
-    <div class="hidden md:flex items-center mx-auto whitespace-nowrap">
-        <span class="text-sm font-semibold text-gray-700 dark:text-gray-200" style="padding-left: 2rem;">
+    <div class="hidden md:flex items-center whitespace-nowrap ms-4">
+        <span class="text-lg font-semibold text-gray-700 dark:text-gray-200">
             Welcome ! {{ trim(auth()->user()->front_title . ' ' . auth()->user()->name . ' ' . auth()->user()->back_title) }}
         </span>
     </div>
