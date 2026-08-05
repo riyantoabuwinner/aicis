bde3d1ddfa146285e0267be78f5f51d1c903ad95
riyantoabuwinner <riyantoabuwinner@example.com>
2026-08-05 17:33:51 +0700

    style: remove account widget (welcome & logout) from dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -65,7 +65,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
                 \App\Filament\Widgets\AuthorGuidelinesWidget::class,
             ])
             ->navigationGroups([
